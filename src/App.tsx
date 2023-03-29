af7834c9242cae3fe263a02dbc9496cdfbd750a4
Rio Del Salas Pamaong <riopamaong@gmail.com>
2023-03-29 15:49:49 +0800

    Initialized the React project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>IONA Test exercise cat application</h1>
+    </div>
+  );
+}
+
+export default App;
