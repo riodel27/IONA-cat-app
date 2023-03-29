14fb4d3b8563d6dd88057168f8527eb3dd206f7c
Rio Del Salas Pamaong <riopamaong@gmail.com>
2023-03-29 18:31:36 +0800

    feat: Set up React-Bootstrap and Styled-Components
    This commit adds the necessary dependencies and configurations to set up React-Bootstrap and Styled-Components in the project.
    
    Closes #IONA-14

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
 
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+const StyledButton = styled(Button)`
+	background-color: #007bff;
+	border-color: #007bff;
+	&:hover {
+		background-color: #0069d9;
+		border-color: #0062cc;
+	}
+`;
+
 function App() {
 	return (
 		<div className="App">
 			<h1>IONA Test exercise cat application</h1>
+
+			<Button variant="danger">React Bootstrap Button</Button>
+
+			<StyledButton>Styled Button</StyledButton>
 		</div>
 	);
 }
