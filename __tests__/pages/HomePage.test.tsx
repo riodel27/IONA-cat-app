496f41bb6a5c47b2f14e3ac65f2ac022b17324cf
Rio Del Salas Pamaong <riopamaong@gmail.com>
2023-03-30 16:04:59 +0800

    Add failing test for the homepage feature when breed is selected

--- __tests__/pages/HomePage.test.tsx
+++ __tests__/pages/HomePage.test.tsx
@@ -1,11 +1,18 @@
-import { describe, test, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, test, expect, vi } from 'vitest';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import axios, { AxiosResponse, AxiosHeaders } from 'axios';
 
 import HomePage from '../../src/pages/HomePage';
 import { Cat } from '../../src/types/types';
 
+vi.mock('axios');
+
+const headers = new AxiosHeaders({
+	'Content-Type': 'application/json',
+});
+
 describe('<HomePage />', () => {
-	const mockData = [
+	const mockCatsData = [
 		{
 			id: '8pCFG7gCV',
 			url: 'https://cdn2.thecatapi.com/images/8pCFG7gCV.jpg',
@@ -20,6 +27,21 @@ describe('<HomePage />', () => {
 		},
 	] as Cat[];
 
+	const mockCatsDataSiameseBreed: Cat[] = [
+		{
+			id: '__tqyLW91',
+			url: 'https://cdn2.thecatapi.com/images/__tqyLW91.jpg',
+			width: 937,
+			height: 1171,
+		},
+		{
+			id: 'KCJeb66J2',
+			url: 'https://cdn2.thecatapi.com/images/KCJeb66J2.jpg',
+			width: 735,
+			height: 1102,
+		},
+	];
+
 	test('HomePage mounts properly', () => {
 		render(<HomePage />);
 
@@ -27,7 +49,7 @@ describe('<HomePage />', () => {
 		expect(selectInput).toBeInTheDocument();
 
 		const catsList = screen.getAllByRole('cat');
-		expect(catsList.length).toBe(mockData.length);
+		expect(catsList.length).toBe(mockCatsData.length);
 
 		expect(catsList[0]).toHaveAttribute(
 			'src',
@@ -41,10 +63,62 @@ describe('<HomePage />', () => {
 		const viewDetailsButton = screen.getAllByRole('button', {
 			name: 'View details',
 		});
-		expect(viewDetailsButton.length).toBe(mockData.length);
+		expect(viewDetailsButton.length).toBe(mockCatsData.length);
 
 		// expect button with load more
 		const loadMoreButton = screen.getByRole('button', { name: 'Load more' });
 		expect(loadMoreButton).toBeInTheDocument();
+	});
+
+	it('should show "No cats available" message if "Select breed" is selected', () => {
+		const { getByLabelText, queryByText } = render(<HomePage />);
+		const selectInput = getByLabelText('Breed');
+
+		fireEvent.change(selectInput, { target: { value: 'Select breed' } });
+
+		expect(queryByText('No cats available')).toBeInTheDocument();
+
+		const catsList = screen.queryAllByRole('cat');
+		expect(catsList).toHaveLength(0);
+	});
+
+	it('should fetch cat data and display it when a breed is selected', async () => {
+		const mockResponse: AxiosResponse<Cat[]> = {
+			data: mockCatsDataSiameseBreed,
+			status: 200,
+			statusText: 'OK',
+			headers,
+			config: {
+				headers,
+			},
+		};
+
+		(axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce(
+			mockResponse
+		);
+
+		const { getByLabelText } = render(<HomePage />);
+
+		const selectInput = getByLabelText('Breed');
+		fireEvent.change(selectInput, { target: { value: 'siam' } });
+
+		await waitFor(() => {
+			const catsList = screen.queryAllByRole('cat');
+			expect(catsList).toHaveLength(mockCatsDataSiameseBreed.length);
+
+			expect(catsList[0]).toHaveAttribute(
+				'src',
+				'https://cdn2.thecatapi.com/images/__tqyLW91.jpg'
+			);
+			expect(catsList[1]).toHaveAttribute(
+				'src',
+				'https://cdn2.thecatapi.com/images/KCJeb66J2.jpg'
+			);
+
+			const viewDetailsButton = screen.getAllByRole('button', {
+				name: 'View details',
+			});
+			expect(viewDetailsButton.length).toBe(mockCatsData.length);
+		});
 	});
 });
